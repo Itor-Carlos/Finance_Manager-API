fdce07c60135b6a458d9ab5273f7721b3079811f
Itor-Carlos <itor51carlos@gmail.com>
2021-11-02 09:16:36 -0300

    declarada DespesaRepository com anotacao Repository

--- src/main/java/com/api/dev/finance_manager/repositories/DespesaRepository.java
+++ src/main/java/com/api/dev/finance_manager/repositories/DespesaRepository.java
@@ -2,6 +2,8 @@ package com.api.dev.finance_manager.repositories;
 
 import com.api.dev.finance_manager.model.Despesa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DespesaRepository extends JpaRepository<Despesa,Long> {
 }
