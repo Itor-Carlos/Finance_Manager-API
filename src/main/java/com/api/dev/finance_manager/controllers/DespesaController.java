ae76cbc809f1aba3b0ce1ff39ca12edc9e922cc5
Itor-Carlos <itor51carlos@gmail.com>
2021-11-02 09:48:08 -0300

    alterado path no RequestMapping da classe DespesaController

--- src/main/java/com/api/dev/finance_manager/controllers/DespesaController.java
+++ src/main/java/com/api/dev/finance_manager/controllers/DespesaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/")
+@RequestMapping(path = "/despesas")
 public class DespesaController {
 
     @Autowired
