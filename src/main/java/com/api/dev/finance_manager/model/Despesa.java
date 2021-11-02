39422a7b2013b0829122f67174b88a6133915bfd
Itor-Carlos <itor51carlos@gmail.com>
2021-11-02 09:01:31 -0300

    inserido atributos, getters, setters e construtores da classe Despesa

--- src/main/java/com/api/dev/finance_manager/model/Despesa.java
+++ src/main/java/com/api/dev/finance_manager/model/Despesa.java
@@ -1,4 +1,43 @@
 package com.api.dev.finance_manager.model;
 
+import java.util.Date;
+
 public class Despesa {
+
+    private Long id;
+    private String destino;
+    private Date data;
+
+    public Despesa() {
+    }
+
+    public Despesa(Long id, String destino, Date data) {
+        this.id = id;
+        this.destino = destino;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
 }
