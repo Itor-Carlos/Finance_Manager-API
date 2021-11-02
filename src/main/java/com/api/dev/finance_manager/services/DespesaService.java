4fd3b538a079185c556ef14b927ef1ee57f2891b
Itor-Carlos <itor51carlos@gmail.com>
2021-11-02 10:18:08 -0300

    criado e implementado metodo buscar na classde DespesaService

--- src/main/java/com/api/dev/finance_manager/services/DespesaService.java
+++ src/main/java/com/api/dev/finance_manager/services/DespesaService.java
@@ -5,7 +5,10 @@ import com.api.dev.finance_manager.repositories.DespesaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class DespesaService {
@@ -17,5 +20,14 @@ public class DespesaService {
         return this.despesaRepository.findAll();
     }
 
-
+    public Despesa buscar(Long id) {
+        if(id < 1){
+            throw new IllegalArgumentException("the id most be higher or equals 1");
+        }
+        Optional<Despesa> todo = this.despesaRepository.findById(id);
+        if(!todo.isPresent()){
+            throw new NoSuchElementException("No value present in this id");
+        }
+        return todo.get();
+    }
 }
