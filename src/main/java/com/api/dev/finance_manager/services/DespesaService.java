d0042f9ce78aef1a1e8613bab17640ab5d3ff77d
Itor-Carlos <itor51carlos@gmail.com>
2021-11-02 09:31:23 -0300

    declarada injecao de dependencia na classe DespesaService

--- src/main/java/com/api/dev/finance_manager/services/DespesaService.java
+++ src/main/java/com/api/dev/finance_manager/services/DespesaService.java
@@ -1,7 +1,14 @@
 package com.api.dev.finance_manager.services;
 
+import com.api.dev.finance_manager.repositories.DespesaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DespesaService {
+
+    @Autowired
+    private DespesaRepository despesaRepository;
+
+
 }
