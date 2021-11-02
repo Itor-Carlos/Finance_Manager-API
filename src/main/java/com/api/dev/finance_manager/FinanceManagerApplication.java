fec1984c7911488ca3f29080c06cf67eed0b6fcc
Itor-Carlos <itor51carlos@gmail.com>
2021-11-01 22:24:27 -0300

    projeto inicializado

---
+++ src/main/java/com/api/dev/finance_manager/FinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.api.dev.finance_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/dev/finance_manager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.dev.finance_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
