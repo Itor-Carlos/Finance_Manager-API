fca6686602a6e88c4a4fa5c3627f0ea319d3891c
Itor-Carlos <itor51carlos@gmail.com>
2021-11-23 23:27:30 -0300

    adicionado metodo find na interface DespesaRepository

--- src/main/java/com/api/dev/finance_manager/repositories/repository/DespesaRepository.java
+++ src/main/java/com/api/dev/finance_manager/repositories/repository/DespesaRepository.java
@@ -4,10 +4,12 @@ import com.api.dev.finance_manager.model.Despesa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface DespesaRepository extends JpaRepository<Despesa,Long> {
 
+    public List<Despesa> find(Long id, String destino, Date data);
 
 }
