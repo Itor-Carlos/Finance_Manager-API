7a4fcfed3f511735acedc5f1e6170aa0ac4b0f80
Itor-Carlos <itor51carlos@gmail.com>
2021-12-16 18:03:44 -0300

    adicionado parametro do tipo DespesaStatus no metodo find

--- src/main/java/com/api/dev/finance_manager/repositories/repository/DespesaRepository.java
+++ src/main/java/com/api/dev/finance_manager/repositories/repository/DespesaRepository.java
@@ -1,5 +1,6 @@
 package com.api.dev.finance_manager.repositories.repository;
 
+import com.api.dev.finance_manager.enums.DespesaStatus;
 import com.api.dev.finance_manager.model.Despesa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,6 @@ import java.util.List;
 @Repository
 public interface DespesaRepository extends JpaRepository<Despesa,Long> {
 
-    public List<Despesa> find(Long id, String destino, Date data);
+    public List<Despesa> find(Long id, String destino, Date data, DespesaStatus despesaStatus);
 
 }
