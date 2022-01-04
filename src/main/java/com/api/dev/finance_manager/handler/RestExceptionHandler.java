036633db2bdee19d9ef7fbbebb8734a239a57b8a
Itor-Carlos <itor51carlos@gmail.com>
2022-01-04 17:30:52 -0300

    criado metodo illegalArgumentException para tratar e devolver uma resposta quando ocorrer uma exceção do tipo IllegalArgumentException

--- src/main/java/com/api/dev/finance_manager/handler/RestExceptionHandler.java
+++ src/main/java/com/api/dev/finance_manager/handler/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.api.dev.finance_manager.handler;
 
 import com.api.dev.finance_manager.exceptions.DespesaNotFoundException;
 import com.api.dev.finance_manager.exceptions.DespesaNotFoundExceptionDetails;
+import com.api.dev.finance_manager.exceptions.IllegalArgumentExceptionDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,4 +23,16 @@ public class RestExceptionHandler {
         );
         return new ResponseEntity<>(despesaNotFoundDetails,HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<IllegalArgumentExceptionDetails> illegalArgumentException(IllegalArgumentException illegalArgumentException){
+        IllegalArgumentExceptionDetails illegalArgumentExceptionDetails = new IllegalArgumentExceptionDetails(
+                illegalArgumentException.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now(),
+                "Illegal Argument"
+        );
+        return new ResponseEntity<>(illegalArgumentExceptionDetails,HttpStatus.BAD_REQUEST);
+    }
+
 }
