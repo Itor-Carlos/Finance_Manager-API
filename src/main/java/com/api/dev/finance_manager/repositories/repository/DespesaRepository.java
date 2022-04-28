1e4439465a5252316a8ee0edc46c31f181ed22fc
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2022-04-27 21:19:49 -0300

    add parameter of type Pageable in method find of interface DespesaRepository and changed the type of return to Page

--- src/main/java/com/api/dev/finance_manager/repositories/repository/DespesaRepository.java
+++ src/main/java/com/api/dev/finance_manager/repositories/repository/DespesaRepository.java
@@ -3,6 +3,9 @@ package com.api.dev.finance_manager.repositories.repository;
 import com.api.dev.finance_manager.enums.DespesaCategoria;
 import com.api.dev.finance_manager.enums.DespesaStatus;
 import com.api.dev.finance_manager.model.Despesa;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +16,7 @@ import java.util.List;
 @Repository
 public interface DespesaRepository extends JpaRepository<Despesa,Long> {
 
-    public List<Despesa> find(Long id, String destino, Date data, DespesaStatus despesaStatus, DespesaCategoria despesaCategoria);
+    public Page<Despesa> find(Long id, String destino, Date data, DespesaStatus despesaStatus, DespesaCategoria despesaCategoria, Pageable pageable);
 
     @Transactional
     public void updateDespesa(Long id, Despesa despesa);
