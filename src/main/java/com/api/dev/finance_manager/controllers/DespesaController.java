8947ffac070d66ad9442b76de5ec1e81d5bb7c50
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2022-05-02 17:39:12 -0300

    added hateoas in method find from DespesaController class

--- src/main/java/com/api/dev/finance_manager/controllers/DespesaController.java
+++ src/main/java/com/api/dev/finance_manager/controllers/DespesaController.java
@@ -1,5 +1,9 @@
 package com.api.dev.finance_manager.controllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+
 import com.api.dev.finance_manager.dto.DespesaDTO;
 import com.api.dev.finance_manager.enums.DespesaCategoria;
 import com.api.dev.finance_manager.enums.DespesaStatus;
@@ -8,6 +12,7 @@ import com.api.dev.finance_manager.model.Despesa;
 import com.api.dev.finance_manager.services.DespesaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
@@ -62,7 +67,13 @@ public class DespesaController {
 
     @GetMapping(path = "/find", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> find(@RequestParam(name = "id", required = false) Long id, @RequestParam(name = "destino",required = false) String destino, @RequestParam(name = "data", required = false) Date data, @RequestParam(name = "despesaStatus",required = false)DespesaStatus despesaStatus, @RequestParam(name = "despesaCategoria",required = false)DespesaCategoria despesaCategoria, Pageable pageable){
-        Page<Despesa> listaResultado = this.despesaService.find(id,destino,data,despesaStatus,despesaCategoria, pageable);
-        return ResponseEntity.ok(listaResultado);
+        Page pageResult = this.despesaService.find(id, destino, data, despesaStatus, despesaCategoria, pageable);
+        List<Despesa> listResult = pageResult.getContent();
+        for(Despesa element: listResult){
+            long idDespesa = element.getId();
+            element.add(linkTo(methodOn(DespesaController.class).buscarForId(idDespesa)).withSelfRel());
+        }
+        pageResult = new PageImpl<>(listResult, pageable, listResult.size());
+        return ResponseEntity.ok(pageResult);
     }
 }
