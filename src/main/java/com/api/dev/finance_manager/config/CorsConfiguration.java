4a55e50042846186463dd4dd0c76df1d0177abcf
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2022-05-12 14:14:54 -0300

    CORS configuration

---
+++ src/main/java/com/api/dev/finance_manager/config/CorsConfiguration.java
@@ -0,0 +1,11 @@
+package com.api.dev.finance_manager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer{
+    
+    
+}
