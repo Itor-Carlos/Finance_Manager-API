2b8113afedc547bf0eb31b16eec064083c53ff07
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2022-07-25 21:55:19 -0300

    added a base package in RestExceptionHandler.java file

--- src/main/java/com/api/dev/finance_manager/handler/RestExceptionHandler.java
+++ src/main/java/com/api/dev/finance_manager/handler/RestExceptionHandler.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.api.dev.finance_manager.controllers")
 public class RestExceptionHandler {
 
     @ExceptionHandler(DespesaNotFoundException.class)
